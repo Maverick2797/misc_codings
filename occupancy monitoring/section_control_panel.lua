2444ec4a8ced0d74da26b9677cb157d78f5b626e
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-07-29 20:15:44 +0800

    add/update to line operability

--- occupancy monitoring/section_control_panel.lua
+++ occupancy monitoring/section_control_panel.lua
@@ -1,8 +1,9 @@
 if event.punch or (event.channel=="clock" and event.msg=="done") then
-local info = S.section_monitor
+local line = "S23" --set line # to monitor
+local info = S.section_monitor.line
 	for _,v in pairs(info) do
 		local msg = "Section: "..v.name.."||Occupancy: "..tostring(v.occupancy).."||Train ID: "..tostring(v.id)
 		digiline_send(v.name,msg)
 	end
     -- digiline_send("CCnr",tostring(S.section_monitor.CCnr.occupancy))
-end
+end	
