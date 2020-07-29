e73b5023b1b48a06d8443dff627a5aff039dd049
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-07-29 20:21:43 +0800

    fix derp
    would have looked for the table "line" instead of the tavbe set by variable "line"

--- occupancy monitoring/section_control_panel.lua
+++ occupancy monitoring/section_control_panel.lua
@@ -1,5 +1,5 @@
 if event.punch or (event.channel=="clock" and event.msg=="done") then
 local line = "S23" --set line # to monitor
-local info = S.section_monitor.line
+local info = S.section_monitor[line]
 	digiline_send("status_update",info)
 end	
