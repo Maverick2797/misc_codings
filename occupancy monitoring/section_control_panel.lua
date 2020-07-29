957346654f3b3640c5f58a6f643927bafb2eebed
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-07-29 20:19:21 +0800

    revert compatability to server-friendly
    send the table as a single messsage instead of a message per section

--- occupancy monitoring/section_control_panel.lua
+++ occupancy monitoring/section_control_panel.lua
@@ -1,9 +1,5 @@
 if event.punch or (event.channel=="clock" and event.msg=="done") then
 local line = "S23" --set line # to monitor
 local info = S.section_monitor.line
-	for _,v in pairs(info) do
-		local msg = "Section: "..v.name.."||Occupancy: "..tostring(v.occupancy).."||Train ID: "..tostring(v.id)
-		digiline_send(v.name,msg)
-	end
-    -- digiline_send("CCnr",tostring(S.section_monitor.CCnr.occupancy))
+	digiline_send("status_update",info)
 end	
